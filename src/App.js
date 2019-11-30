a7d5f7f595631e376b7b5fec4d27232479a4be22
Rafael <rafael_guliev94@list.ru>
2019-12-01 02:08:47 +0300

    environment setting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import styles from "./App.scss";
+import DATA from './apps';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null
+        }
+    }
+
+    componentDidMount() {
+        console.log(DATA);
+    }
+
+
+    render() {
+        return (
+            <div className={styles.App}>
+                asdasd
+            </div>
+        );
+    }
 }
-
-export default App;
---
+++ src/components/Card/Card.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import styles from './Card.scss';
+
+export default class Card extends Component {
+    render() {
+        return(
+            <div>123</div>
+        )
+    }
+}
