1a55f6a40a8bcfd134434ddf1acff00af42d9e0f
Rafael <rafael_guliev94@list.ru>
2019-12-21 01:31:27 +0300

    add store

--- src/App.js
+++ src/App.js
@@ -1,87 +1,73 @@
 import React, {Component} from 'react';
 import styles from "./App.scss";
-import DATA from './apps';
 import Card from "./components/Card/Card";
 import Filter from "./components/Filter/Filter";
 import {SHOW_ALL} from "./actions/actions";
+import {connect} from 'react-redux';
 
-import {createStore} from "redux";
-
-function appList(state = [DATA], action) {
-    if (action.type === "SHOW_ALL") {
-        return [
-            ...state,
-            action.payload
-        ]
-    }
-
-    return state;
-}
-
-const store = createStore(appList);
-
-store.subscribe(()=> {
-    console.log('subscribe', store.getState());
-});
-
-store.dispatch({type: "SHOW_ALL", payload: "Show all cards"});
-
-export default class App extends Component {
+class App extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            data: DATA,
-            games: false,
-            tinkoff: true,
-            filter: 'all',
-            currentData: null
-        }
     }
 
     componentDidMount() {
-        const {data, games, tinkoff, currentData} = this.state;
-
-        if (games) {
-            this.setState({
-                currentData: data.games
-            })
-        } else if (tinkoff) {
-            this.setState({
-                currentData: data.tinkoff
-            })
-        }
     }
 
-
-
     render() {
-        const {data, games, tinkoff, filter, currentData} = this.state;
+        // const {data, games, tinkoff, filter, currentData} = this.state;
+        // let cardsArr;
+        // if (currentData !== null) {
+        //     let id = 0;
+        //     for (let item of currentData.applications) {
+        //         item.key = id;
+        //         id += 1;
+        //     }
+        //     cardsArr = currentData.applications.map((item)=> {
+        //         return <Card
+        //             key={item.key}
+        //             appAvatar={item.icon}
+        //             appName={item.name}
+        //             appPlatform={item.platform}
+        //             price={item.price}
+        //             rating={item.rating}
+        //             location={item.location}
+        //             aso={'ASO index'}
+        //             asoStats={item.asoindex}
+        //             installs={'Установок в месяц'}
+        //             installsStats={item.installations}
+        //             category={'В категории'}
+        //             categoryStats={item.categoryPosition}
+        //         />
+        //     });
+        // }
+
+        console.log(this.props.store)
+        const {tinkoff} = this.props.store[0];
+
         let cardsArr;
-        if (currentData !== null) {
-            let id = 0;
-            for (let item of currentData.applications) {
-                item.key = id;
-                id += 1;
-            }
-            cardsArr = currentData.applications.map((item)=> {
-                return <Card
-                    key={item.key}
-                    appAvatar={item.icon}
-                    appName={item.name}
-                    appPlatform={item.platform}
-                    price={item.price}
-                    rating={item.rating}
-                    location={item.location}
-                    aso={'ASO index'}
-                    asoStats={item.asoindex}
-                    installs={'Установок в месяц'}
-                    installsStats={item.installations}
-                    category={'В категории'}
-                    categoryStats={item.categoryPosition}
-                />
-            });
+        let id = 0;
+        for (let item of tinkoff.applications) {
+            item.key = id;
+            id += 1;
         }
+        cardsArr = tinkoff.applications.map((item)=> {
+            return <Card
+                key={item.key}
+                appAvatar={item.icon}
+                appName={item.name}
+                appPlatform={item.platform}
+                price={item.price}
+                rating={item.rating}
+                location={item.location}
+                aso={'ASO index'}
+                asoStats={item.asoindex}
+                installs={'Установок в месяц'}
+                installsStats={item.installations}
+                category={'В категории'}
+                categoryStats={item.categoryPosition}
+            />
+        });
+
         return (
             <div className={styles.App}>
                 <Filter
@@ -92,3 +78,10 @@ export default class App extends Component {
         );
     }
 }
+
+export default connect(
+    state => ({
+        store: state
+    }),
+    dispatch => ({})
+)(App);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import DATA from './apps';
 import * as serviceWorker from './serviceWorker';
+import {Provider, connect} from 'react-redux';
+import {createStore} from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function appList(state = [DATA], action) {
+    if (action.type === "SHOW_ALL") {
+        return [
+            ...state,
+            action.payload
+        ]
+    }
+
+    return state;
+}
+
+const store = createStore(appList);
+
+store.subscribe(()=> {
+    console.log('subscribe', store.getState());
+});
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
