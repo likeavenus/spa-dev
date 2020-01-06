9b3034d0a49ebf0b65993897b11f579279f1a955
Rafael <rafael_guliev94@list.ru>
2020-01-06 16:28:59 +0300

    Updates

---
+++ precache-manifest.31ed6dc3f1449bca28af9ae131e2b02d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4275eb95755332340ed6c1aa1affe34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb2887f16fa01fb7a30",
+    "url": "/static/css/main.8676f885.chunk.css"
+  },
+  {
+    "revision": "e308c864c7bf96f2fbdc",
+    "url": "/static/js/2.a57c5a29.chunk.js"
+  },
+  {
+    "revision": "8cb2887f16fa01fb7a30",
+    "url": "/static/js/main.93f92b7e.chunk.js"
+  },
+  {
+    "revision": "2cf27c8060098ea495da",
+    "url": "/static/js/runtime-main.4b4a6dac.js"
+  },
+  {
+    "revision": "07a8a8ce3f4f7e714350c2529f070578",
+    "url": "/static/media/MuseoSansCyrl-500.07a8a8ce.woff"
+  },
+  {
+    "revision": "5736894baf0c6f859b1f5f7fa4ac83dc",
+    "url": "/static/media/MuseoSansCyrl-900.5736894b.woff"
+  },
+  {
+    "revision": "8b8b9a1a23698ce308deea81bd6e6742",
+    "url": "/static/media/arrow-down-1.8b8b9a1a.svg"
+  }
+]);
